1ebf28a8a56e46ca4d75cf495b0894db314af733
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-01-07 13:36:38 -0800

    Added Clerk Authentication

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,12 +1,11 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
 const DashboardPage = () => {
   return (
     <>
       <p>Dashboard Page(Protected)</p>
-      <Button variant="default" size="lg">
-        Click Me
-      </Button>
+      <UserButton afterSignOutUrl="/" />
     </>
   );
 };
