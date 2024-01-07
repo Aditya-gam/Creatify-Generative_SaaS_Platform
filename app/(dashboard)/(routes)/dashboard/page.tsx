75b750d38416e283b3599742c92c321f8e4ebe16
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-01-07 12:46:55 -0800

    Folder Setup

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <>
+      <p>Dashboard Page(Protected)</p>
+      <Button variant="default" size="lg">
+        Click Me
+      </Button>
+    </>
+  );
+};
+
+export default DashboardPage;
