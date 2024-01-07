b9f80dee12838dec994b40d07f51e8bebaee768b
Aditya Gambhir <67105262+Aditya-gam@users.noreply.github.com>
2024-01-07 14:02:46 -0800

    Added Basic Navbar Component

--- app/(dashboard)/(routes)/dashboard/page.tsx
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
-
 const DashboardPage = () => {
   return (
     <>
       <p>Dashboard Page(Protected)</p>
-      <UserButton afterSignOutUrl="/" />
     </>
   );
 };
